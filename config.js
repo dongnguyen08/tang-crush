0a1c1a868aba3d8f9853cd7ef93453f43f7db050
dongnguyen08 <85278907+dongnguyen08@users.noreply.github.com>
2021-06-03 17:23:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, anh em thôi :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao May lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao  lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu  nhiều nhiều 😘😘',
